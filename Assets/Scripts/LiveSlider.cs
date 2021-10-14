b477f238a3af13c0edd6951cbc35bc5b09382559
JWang-912 <justin.wang91202@gmail.com>
2021-10-14 17:50:02 -0400

    Unfinished LiveSlider script

---
+++ Assets/Scripts/LiveSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveSlider : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject display;
+    Color colorArr[] = new Color[] { Color.green, Color.yellow, Color.cyan, Color.magenta, Color.red};
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.onValueChanged.AddListener(UpdateDisplay);
+    }
+
+    void UpdateDisplay()
+    {
+        Text displayText = display.GetComponent<Text>();
+        displayText.text = slider.value;
+        Image displayImage = display.GetComponent<Image>();
+        displayImage.color = colorArr[slider.value];
+    }
+
+}
